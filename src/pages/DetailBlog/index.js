84615c0a009c20b4b8046d46fdbdfcff8d66ace5
Lutfi Maulana <lutfimaulana.fi@gmail.com>
2020-10-19 20:35:35 +0700

    Styling page detail blog

--- src/pages/DetailBlog/index.js
+++ src/pages/DetailBlog/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { RegisterBg } from '../../assets'
+import './detailBlog.scss'
 
 const DetailBlog = () => {
     return (
-        <div>
-            <p>Content detail blog page</p>
+        <div className="detail-blog-wrapper">
+            <img className="img-cover" src={RegisterBg} alt="thumb" />
+            <p className="blog-title">Title Blog</p>
+            <p className="blog-author">Author - Date Post</p>
+            <p className="blog-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum quam sapien, congue nec turpis a, imperdiet dapibus diam. Morbi fringilla aliquam laoreet. Vestibulum non lacus at neque lacinia egestas. Duis finibus porttitor lacus, et dapibus eros commodo lacinia. Aliquam id enim in velit pellentesque luctus commodo at ex. Vestibulum fermentum auctor mauris eu lacinia. Nulla luctus eget sem eget lacinia. Vivamus mi nisi, pretium non nisl quis, placerat vulputate leo. Sed libero turpis, euismod ac aliquam id, imperdiet at quam. Pellentesque est sapien, vestibulum in urna vitae, ullamcorper sollicitudin massa. Ut tincidunt nisl et turpis eleifend, vitae auctor nulla accumsan. Duis mollis rutrum metus. Curabitur fringilla tellus eu ex venenatis, ac pellentesque felis eleifend. Suspendisse potenti.</p>
         </div>
     )
 }
