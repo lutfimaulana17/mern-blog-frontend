9b778f21fd3dc123862d85826b648cfbd0a1e716
Lutfi Maulana <lutfimaulana.fi@gmail.com>
2020-10-19 20:19:11 +0700

    Styling page create blog

--- src/pages/CreateBlog/index.js
+++ src/pages/CreateBlog/index.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Button, Gap, Input, TextArea, Upload } from '../../components'
+import './createBlog.scss'
 
 const CreateBlog = () => {
     return (
-        <div>
-            <p>Content Create Blog Page</p>
+        <div className="blog-post">
+            <p className="title">Create New Blog Post</p>
+            <Input label="Post Title" />
+            <Upload />
+            <TextArea />
+            <Gap height={20} />
+            <div className="button-action">
+                <Button title="Save" />
+            </div>
+            <Gap height={20} />
         </div>
     )
 }
